3db5221830410416cc25472b272a489ce91ffbf4
ferlg <ferlory17@gmail.com>
2025-03-15 18:10:09 -0600

    Initial Commit

---
+++ GOW/GOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOWApp.swift
+//  GOW
+//
+//  Created by Macbook Air on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GOWTests/GOWTests.swift
@@ -0,0 +1,17 @@
+//
+//  GOWTests.swift
+//  GOWTests
+//
+//  Created by Macbook Air on 15/03/25.
+//
+
+import Testing
+@testable import GOW
+
+struct GOWTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
